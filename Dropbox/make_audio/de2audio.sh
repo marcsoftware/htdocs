208426a92b53b7b633d40032f85e228d8038a1d7
marc <marcdavidmelcher@gmail.com>
2018-12-08 16:49:59 -0600

    feature:made audio files.

--- Dropbox/make_audio/de2audio.sh
+++ Dropbox/make_audio/de2audio.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # use this command to run
-# ./check.sh < input.txt
+# ./de2audio.sh < input.txt
+#requites HEDDA voice to be installed. this can be done in 'region and language settings:german'
+
+
+
+
+
 
 # returns 1 if the file not there or is empty
 doesExist (){
-    _file="$1.wav"
+    _file="../audio-de/$1.wav"
     [ $# -eq 0 ] && { echo "Usage: $1 filename"; return 1; }
     [ ! -f "$_file" ] && { echo "$1 file not found."; return 1; }
 
@@ -30,6 +36,7 @@ makeEspeak (){
 
 makeWAV (){
   PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "./makeWAV.ps1 '$1'"
+  #powershell -ExecutionPolicy ByPass -File makeWAV.ps1
 
 }
 
@@ -44,7 +51,7 @@ do
    then
       echo "    $word  - wav already exsists";
    else
-      makeWAV $word
+      makeWAV "$word"
 
       if  doesExist "$word.wav"
          then
@@ -55,3 +62,5 @@ do
       fi
    fi
 done
+
+makeWAV "marc"
